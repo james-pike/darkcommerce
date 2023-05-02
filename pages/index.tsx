052ba8fad886b3c3d02ab6dbc87d4a0a7a10d869
James Pike <jamesandrewpike@gmail.com>
2023-05-01 23:46:12 -0400

    Commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,23 +14,18 @@ const fnavigation = {
     { name: 'Marketing', href: '#' },
     { name: 'Analytics', href: '#' },
     { name: 'Commerce', href: '#' },
-    { name: 'Insights', href: '#' },
   ],
   support: [
     { name: 'Pricing', href: '#' },
     { name: 'Documentation', href: '#' },
     { name: 'Guides', href: '#' },
-    { name: 'API Status', href: '#' },
   ],
   company: [
     { name: 'About', href: '#' },
-    { name: 'Blog', href: '#' },
-    { name: 'Jobs', href: '#' },
     { name: 'Press', href: '#' },
     { name: 'Partners', href: '#' },
   ],
   legal: [
-    { name: 'Claim', href: '#' },
     { name: 'Privacy', href: '#' },
     { name: 'Terms', href: '#' },
   ],
@@ -746,61 +741,120 @@ export default function Example() {
         </section>
       </main>
 
-  
 
-      <footer className="bg-gray-900" aria-labelledby="footer-heading">
-  <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-12">
-    <div className="bg-gray-900 sm:py-24 lg:py-16 mt-16">
-      <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 px-6 lg:grid-cols-12 lg:gap-8 lg:px-8">
-        <div className="max-w-xl text-3xl font-medium tracking-tight text-white sm:text-4xl lg:col-span-7 mt-0">
-          <h2 className="inline sm:block lg:inline xl:block">Want product news and updates?</h2>{' '}
-          <p className="inline sm:block lg:inline xl:block">Sign up for our newsletter.</p>
+  
+    <footer className="bg-gray-900" aria-labelledby="footer-heading">
+      <h2 id="footer-heading" className="sr-only">
+        Footer
+      </h2>
+      <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
+        <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+          <img
+            className="h-7"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+            alt="Company name"
+          />
+          <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-semibold leading-6 text-white">Solutions</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {fnavigation.solutions.map((item) => (
+                    <li key={item.name}>
+                      <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold leading-6 text-white">Support</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {fnavigation.support.map((item) => (
+                    <li key={item.name}>
+                      <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="md:grid md:grid-cols-2 md:gap-8">
+              <div>
+                <h3 className="text-sm font-semibold leading-6 text-white">Company</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {fnavigation.company.map((item) => (
+                    <li key={item.name}>
+                      <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-10 md:mt-0">
+                <h3 className="text-sm font-semibold leading-6 text-white">Legal</h3>
+                <ul role="list" className="mt-6 space-y-4">
+                  {fnavigation.legal.map((item) => (
+                    <li key={item.name}>
+                      <a href={item.href} className="text-sm leading-6 text-gray-300 hover:text-white">
+                        {item.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-        <form className="w-full max-w-md lg:col-span-5 lg:pt-2">
-          <div className="flex gap-x-4">
+        <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24 lg:flex lg:items-center lg:justify-between">
+        <div>
+  <h3 className="text-sm font-semibold leading-6 text-white text-center lg:text-left">Subscribe to our newsletter</h3>
+  <p className="mt-2 text-sm leading-6 text-gray-300 text-center lg:text-left">
+    The latest news, articles, coupons, and resources, sent directly to your inbox weekly.
+  </p>
+</div>
+          <form className="mt-6 sm:flex sm:max-w-md lg:mt-0">
             <label htmlFor="email-address" className="sr-only">
               Email address
             </label>
             <input
-              id="email-address"
-              name="email"
               type="email"
+              name="email-address"
+              id="email-address"
               autoComplete="email"
               required
-              className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+              className="w-full min-w-0 appearance-none rounded-md border-0 bg-white/5 px-3 py-1.5 text-base text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:w-56 sm:text-sm sm:leading-6"
               placeholder="Enter your email"
             />
-            <button
-              type="submit"
-              className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-            >
-              Subscribe
-            </button>
+            <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
+              <button
+                type="submit"
+                className="flex w-full items-center justify-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+              >
+                Subscribe
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="mt-8 border-t border-white/10 pt-8 md:flex md:items-center md:justify-between">
+          <div className="flex space-x-6 md:order-2 justify-center">
+            {fnavigation.social.map((item) => (
+              <a key={item.name} href={item.href} className="text-gray-500 hover:text-gray-400">
+                <span className="sr-only">{item.name}</span>
+                <item.icon className="h-6 w-6" aria-hidden="true" />
+              </a>
+            ))}
           </div>
-          <p className="mt-4 text-sm leading-6 text-gray-300 hidden sm:block">
-            We'll protect your data. Read our{' '}
-            <a href="#" className="font-semibold text-white">
-              privacy&nbsp;policy
-            </a>
+          <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0 text-center">
+            &copy; 2020 Your Company, Inc. All rights reserved.
           </p>
-        </form>
+        </div>
       </div>
-    </div>
-    <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
-      <div className="flex space-x-6 md:order-2 justify-center">
-        {fnavigation.social.map((item) => (
-          <a key={item.name} href={item.href} className="text-gray-500 hover:text-gray-400">
-            <span className="sr-only">{item.name}</span>
-            <item.icon className="h-6 w-6" aria-hidden="true" />
-          </a>
-        ))}
-      </div>
-      <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0 text-center">
-        &copy; 2020 Your Company, Inc. All rights reserved.
-      </p>
-    </div>
-  </div>
     </footer>
+  
     </div>
   )
 }
